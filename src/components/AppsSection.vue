471d69c70b4a674978dc1872cd16f6e033b66a26
Sergey Lisovskiy <lisovskiyserj@gmail.com>
2024-07-05 16:36:13 +0400

    feat: hide telegram app

--- src/components/AppsSection.vue
+++ src/components/AppsSection.vue
@@ -7,15 +7,11 @@ const items = [{
   title: 'Fearless Wallet',
   href: 'https://fearlesswallet.io',
   image: { src: '/apps/fearless-wallet.jpg', alt: 'Fearless Wallet mobile app' }
-}, {
-  title: 'Telegram App',
-  href: 'https://t.me/PolkaswapDevBot/app',
-  image: { src: '/apps/telegram-app.jpg', alt: 'Polkaswap Telegram app' }
 }]
 </script>
 
 <template>
-  <section class="mt-xxl px-4xs">
+  <section class="mt-xxl">
     <h2 class="text-xxl color-semi text-center w-narrow mx-auto mb-s px-s">
       Polkaswap on the <span class="color-accent">Go</span>
     </h2>
@@ -23,7 +19,7 @@ const items = [{
       You can also swap and pool with style and freedom from the palm of your hand via the SORA Wallet and Fearless
       Wallet apps!
     </p>
-    <div class="tiles grid gap-4xs mt-xl">
+    <div class="tiles grid gap-4xs mt-xl w px-s mx-auto">
       <Tile v-for="item, in items" v-bind="item" target="_blank" />
     </div>
   </section>
@@ -32,7 +28,7 @@ const items = [{
 <style scoped>
 @media (min-width: 800px) {
   .tiles {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-template-columns: repeat(2, minmax(0, 1fr));
   }
 }
 </style>
